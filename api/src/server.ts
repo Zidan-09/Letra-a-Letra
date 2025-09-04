8e113d9b2a5a7990910e402a9df0429e2df1b9d4
Zidan-09 <samuel.profissional18@gmail.com>
2025-09-04 08:15:21 -0300

    UPDATE - Server initialization now with sync function and socket

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,18 @@
 import { createServer } from "http";
 import app from "./app";
+import { initSocket } from "./socket";
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3333;
 
-const httpServer = createServer(app);
+function startServer() {
+    const httpServer = createServer(app);
+    
+    initSocket(httpServer);
 
-httpServer.listen(PORT, () => {
-    console.log(`Server Running on: http://localhost:${PORT}`)
-})
+    httpServer.listen(PORT, () => {
+        console.log(`Server Running on: http://localhost:${PORT}`)
+    })
+}
+
+startServer();
