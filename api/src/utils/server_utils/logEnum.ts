d5317112db3ad9a9027333facf1dc60e19e6e5e3
Zidan-09 <samuel.profissional18@gmail.com>
2025-09-09 13:43:20 -0300

    Some files who not be commited in last commit

---
+++ api/src/utils/server_utils/logEnum.ts
@@ -0,0 +1,10 @@
+export enum LogEnum {
+    RoomCreated = "room created",
+    RoomClosed = "room closed",
+    PlayerJoinned = "joinned the game",
+    PlayerReconnected = "reconnected the game",
+    PlayerLeft = "left the game",
+    PlayerReveal = "reveal in",
+    GameStarted = "game started",
+    GameOver = "game over",
+}
