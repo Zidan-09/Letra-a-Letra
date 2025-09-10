aa53bb706473a5a42c402291be32cefa8233c54c
Zidan-09 <samuel.profissional18@gmail.com>
2025-09-10 16:26:33 -0300

    UPDATE - Updated README and update on server url

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,14 +3,17 @@ import cors from 'cors';
 import roomRouter from "./routes/roomRoutes";
 import gameRouter from "./routes/gameRoutes";
 import playerRouter from "./routes/playerRoutes";
+require('dotenv').config();
 
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/room", roomRouter);
-app.use("/game", gameRouter);
-app.use("/player", playerRouter);
+const VERSION: string = process.env.VERSION || "v1"
+
+app.use(`${VERSION}/room`, roomRouter);
+app.use(`${VERSION}/game`, gameRouter);
+app.use(`${VERSION}/player`, playerRouter);
 
 export default app;
